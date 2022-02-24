ba7766c2b93acf06093e7862fa0f4db45f8c0077
savchenko_v <savchenko_v@mail.n.o>
2022-02-24 17:03:34 +0300

    Сортировка пузырьком

---
+++ script.js
@@ -0,0 +1,17 @@
+function sorting(sortList) {
+    for (var i = 0; i < sortList.length-1; i++) {
+        for (var j = 0; j < sortList.length-i; j++) {
+             if (sortList[j+1] < sortList[j]) {
+                 const buffer = sortList[j+1];
+                 sortList[j+1] = sortList[j];
+                 sortList[j] = buffer;
+             }
+            //  console.log(sortList.join(','));
+        }
+        // console.log("------------");
+    }
+}
+
+const sortList = [6, 12, 4, 3, 8, 13, 2, 100, 55, 37];
+sorting(sortList);
+console.log(sortList);
